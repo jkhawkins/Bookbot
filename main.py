53adde25051d6361113ac18e13307bcb592c3e7c
Jack Hawkins <jack_hawkins@btinternet.com>
2025-11-18 21:35:23 +0000

    Argument Update

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from stats import get_num_words, get_letter_nums
+import sys
+from stats import get_num_words, get_letter_nums, sort_letters, letter_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    letter_nums = get_letter_nums(get_book_text("books/frankenstein.txt"))
-    print(f"Found {num_words} total words")
-    print(letter_nums)
+    try:
+        book = sys.argv[1]
+        num_words = get_num_words(get_book_text(book))
+        letter_nums = get_letter_nums(get_book_text(book))
+        report = letter_report(book, num_words, sort_letters(letter_nums))
+        print(report)
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,28 @@ def get_letter_nums(text):
         else:
             letter_nums.update({letter_lower: 1})
     return letter_nums
+
+def sort_on(items):
+    return items["num"]
+
+def sort_letters(raw_letters):
+    letter_list = []
+    for raw_letter in raw_letters:
+        if raw_letter.isalpha():
+            letter_list.append({"char": raw_letter, "num": raw_letters[raw_letter]})
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+
+def letter_report(book, words, letter_list):
+    bot_header = "============ BOOKBOT ============\n"
+    book_text = f"Analyzing book found at {book}...\n"
+    wordcount_header = "----------- Word Count ----------\n"
+    wordcount_text = f"Found {words} total words\n"
+    charcount_header = "--------- Character Count -------\n"
+    charcount_text = ""
+    for letter in letter_list:
+        charcount_text += f"{letter["char"]}: {letter["num"]}\n"
+    footer = "============= END ==============="
+    total_text = bot_header + book_text + wordcount_header + wordcount_text + charcount_header + charcount_text + footer
+    return total_text
+
